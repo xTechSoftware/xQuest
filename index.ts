3abf133df0b7150518e3919ed90feb20eeff12b8
Suni29 <71086259+Suni29@users.noreply.github.com>
2022-10-21 16:52:14 +0000

    Update Files

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 let client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [
+        GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildBans
+    ]
 });
+
+client.login(process.env["TOKEN"]);
